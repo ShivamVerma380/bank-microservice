0d81af0c24089281c0031a020a9b0ec6dd7d1d30
ShivamVerma380 <shivam.r.verma@seagate.com>
2024-03-12 20:36:25 +0530

    Update Audit columns using Spring Data

--- accounts/src/main/java/com/eazybank/accounts/entity/BaseEntity.java
+++ accounts/src/main/java/com/eazybank/accounts/entity/BaseEntity.java
@@ -6,22 +6,34 @@ import lombok.ToString;
 
 import java.time.LocalDateTime;
 
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 
 @MappedSuperclass
 @Getter @Setter @ToString
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {   
 
+    @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
+    @CreatedBy
     @Column(updatable = false)
     private String createdBy;
 
+    @LastModifiedDate
     @Column(insertable = false)
     private String updatedAt;
 
+    @LastModifiedBy
     @Column(insertable = false)
     private String updatedBy;
 }
