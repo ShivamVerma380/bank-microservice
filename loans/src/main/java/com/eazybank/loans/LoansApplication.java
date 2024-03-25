b47cc57ee4c008a2a3e1e3986b3b225bd033715e
ShivamVerma380 <shivam.r.verma@seagate.com>
2024-03-25 08:26:09 +0530

    Added ErrorResponseDto

--- loans/src/main/java/com/eazybank/loans/LoansApplication.java
+++ loans/src/main/java/com/eazybank/loans/LoansApplication.java
@@ -2,8 +2,36 @@ package com.eazybank.loans;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Loans microservice REST API documentation",
+		description = "Eazybank Loans microservice REST API Documentation",
+		version = "v1",
+		contact = @Contact(
+			name = "Shivam Verma",
+			email = "shivamvermasv380@gmail.com",
+			url = "https://github.com/ShivamVerma380"
+		),
+		license = @License(
+			name = "Apache 2.0",
+			url = "https://github.com/ShivamVerma380"
+		)
+	),
+	externalDocs = @ExternalDocumentation(
+		description = "EazyBank Loans microservice REST API Documentation",
+		url = "http://localhost:8090/swagger-ui/index.html"
+	)
+)
 public class LoansApplication {
 
 	public static void main(String[] args) {
---
+++ loans/src/main/java/com/eazybank/loans/dto/ErrorResponseDto.java
@@ -0,0 +1,36 @@
+package com.eazybank.loans.dto;
+
+import org.springframework.http.HttpStatus;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+@Schema(
+    name = "ErrorResponse",
+    description = "Schema to hold error response information"
+)
+@Data @AllArgsConstructor
+public class ErrorResponseDto {
+    
+    @Schema(
+        description = "API path invoked by client"
+    )
+    private String apiPath;
+
+    @Schema(
+        description = "Error response representing the error happened"
+    )
+    private HttpStatus errorCode;
+
+    @Schema(
+        description = "Error message representing the error happened"
+    )
+    private String errorMsg;
+
+    @Schema(
+        description = "Time representing the error happened"
+    )
+    private String errorTime;
+}
