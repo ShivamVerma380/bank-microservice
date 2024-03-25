88e81942711a88c472d84cd5ecaa5fbd89a1af75
ShivamVerma380 <shivam.r.verma@seagate.com>
2024-03-25 20:44:52 +0530

    LoansRepository added

---
+++ loans/src/main/java/com/eazybank/loans/repository/LoansRepository.java
@@ -0,0 +1,14 @@
+package com.eazybank.loans.repository;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.eazybank.loans.entity.Loans;
+import java.util.Optional;
+
+@Repository
+public interface LoansRepository extends JpaRepository<Loans, Long> {
+    
+    Optional<Loans> findByMobileNumber(String mobileNumber);
+
+    Optional<Loans> findByLoanNumber(String loanNumber);
+}
