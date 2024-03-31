3c676cd89f220af821fde8a6e7bc7c7f421c6bf7
sverma <sverma@redseal.net>
2024-03-31 12:20:34 +0530

    Added swagger config in cards application class

--- cards/src/main/java/com/eazybank/cards/CardsApplication.java
+++ cards/src/main/java/com/eazybank/cards/CardsApplication.java
@@ -2,8 +2,36 @@ package com.eazybank.cards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = "EazyBank Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Shivam Verma",
+						email = "shivamvermasv380@gmail.com",
+						url = "https://localhost:9000/swagger-ui/index.html"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://localhost:9000/swagger-ui/index.html"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "EazyBank Cards microservice REST API Documentation",
+				url = "https://localhost:9000/swagger-ui/index.html"
+		)
+)
 public class CardsApplication {
 
 	public static void main(String[] args) {
