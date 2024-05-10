8ea04b7fe3e38092a50176779196f781eda4c8f6
Shivam Verma <shivamvermasv380@gmail.com>
2024-05-10 17:21:56 +0530

    Implemented rate limiter pattern in accounts micro service.

--- accounts/src/main/java/com/eazybank/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/eazybank/accounts/controller/AccountsController.java
@@ -27,6 +27,7 @@ import com.eazybank.accounts.dto.ErrorResponseDto;
 import com.eazybank.accounts.dto.ResponseDto;
 import com.eazybank.accounts.service.IAccountsService;
 
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -225,11 +226,18 @@ public class AccountsController {
             )
         }
     )
+    @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity
             .status(HttpStatus.OK)
             .body(environment.getProperty("HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable t) {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body("JAVA 17");
     }
 
 
